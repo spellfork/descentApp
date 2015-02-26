ef76bf2d94704153a20fbfc24c60f2b0a38d859e
spellfork <joakim.rannikko@chas.se>
2015-02-26 17:27:54 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var SERVER_PORT = 9001;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // configurable paths
+    var appConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
+    
+    grunt.initConfig({
+        watch: {
+            less: {
+                files: ['less/*.less'],
+                tasks: ['less']
+            },
+            html: {
+                files: ['templates/**/*.html']
+            },
+            js: {
+                files: ['script/**/*.js', 'Gruntfile.js']
+            },
+            options: {
+                livereload: true
+            }
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.',
+                        src: ['font/**', 'img/**', 'css/**', 'script/**', 'index.html'],
+                        dest: '<%= appConfig.dist %>'
+                    },
+                    {
+                        src: 'node_modules/apache-server-configs/dist/.htaccess',
+                        dest: '<%= appConfig.dist %>/.htaccess'
+                    }
+                ]
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: './script/',
+                    name: 'main',
+                    out: 'dist/script/main.js',
+                    mainConfigFile: 'script/main.js',
+                    optimize: 'uglify2',
+                    preserveLicenseComments: false,
+                    generateSourceMaps: false
+                }
+            }
+        },
+        less: {
+            options: {
+                sourceMap: true,
+                sourceMapFilename: 'css/style.css.map',
+                sourceMapRootpath: '/'
+            },
+            compile: {
+                files: {
+                    'css/style.css': 'less/style.less'
+                }
+            }
+        },
+        clean: {
+            dist: ['dist']
+        },
+        connect: {
+            server: {
+                options: {
+                    hostname: '*',
+                    port: SERVER_PORT,
+                    base: '.',
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    // load all grunt tasks matching the `grunt-*` patterns
+    require('load-grunt-tasks')(grunt, {pattern: ['grunt-*']});
+
+    grunt.registerTask('default', [
+        'less:compile',
+        'connect',
+        'watch'
+    ]);
+    
+    grunt.registerTask('watching', [
+        'connect',
+        'watch'
+    ]);
+
+    grunt.registerTask('compile', [
+        'clean:dist',
+        'less:compile',
+        'copy:dist'
+    ]);
+
+};
